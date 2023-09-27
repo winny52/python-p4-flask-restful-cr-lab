27c7e643d2bd4923ed917711abc5f54e75d152d6
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-27 21:03:43 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -17,10 +17,52 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+     def get(self):
+        
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+
+        return response
+     def post(self):
+            data = request.get_json()
+            new_record = Plant(
+                name=data['name'],
+                image=data['image'],
+                price=data['price']
+            )
+
+            db.session.add(new_record)
+            db.session.commit()
+
+            response_dict = new_record.to_dict()
+
+            response = make_response(
+                response_dict,
+                201,
+            )
+
+            return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
         
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    image = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Float, nullable=False)
